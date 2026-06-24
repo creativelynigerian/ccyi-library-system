32681b6e462d6e040c584c58b7086b7f9dc02433
creativelynigerian <83108755+creativelynigerian@users.noreply.github.com>
2026-06-24 03:50:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-let books=[];
-function addBook(){
- books.push({
-  title:document.getElementById('title').value,
-  author:document.getElementById('author').value
- });
- render();
-}
-function render(){
- document.getElementById('books').innerHTML=books.map(b=>`<tr><td>${b.title}</td><td>${b.author}</td></tr>`).join('');
-}
+const books=[];function addBook(){books.push({title:title.value,author:author.value});bookTable.innerHTML=books.map(b=>`<tr><td>${b.title}</td><td>${b.author}</td></tr>`).join('');}
