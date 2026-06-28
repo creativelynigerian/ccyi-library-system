9fb086c57f3bd956288be374a989a931bb63ae41
creativelynigerian <83108755+creativelynigerian@users.noreply.github.com>
2026-06-28 01:56:09 +0100

    Initialize Firebase and Firestore
    Added Firebase initialization and Firestore configuration.

--- js/app.js
+++ js/app.js
@@ -769,3 +769,32 @@ window.searchBooks = searchBooks;
 
 init();
 
+
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+import {
+  getFirestore
+}
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeWDl2SM1t50tfxLZE6PWabrjdEnp42kw",
+  authDomain: "library-inventory-system-da62c.firebaseapp.com",
+  projectId: "library-inventory-system-da62c",
+  storageBucket: "library-inventory-system-da62c.firebasestorage.app",
+  messagingSenderId: "856447377350",
+  appId: "1:856447377350:web:8bd2bafd175e21e24c1cc7",
+  measurementId: "G-7WLDLW85Z9"
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+export { db };
+
+
+
